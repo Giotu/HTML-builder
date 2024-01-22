8782a98ce31d1df9b6df0e3678d5fb1cc5eadcba
Giotu <evilalex1998@mail.ru>
2024-01-22 17:47:55 +0300

    feat: resolve second task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,9 +1,22 @@
-const { stdin } = process;
+const { stdin, stdout } = process;
 const fs = require('fs');
 const path = require('path');
 
 const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+stdout.write('Please enter values:\n');
 
 stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    process.exit();
+  }
   output.write(data);
+});
+
+function sayGoodbye() {
+  stdout.write('Time to say goodbye');
+}
+
+process.on('exit', sayGoodbye);
+process.on('SIGINT', () => {
+  process.exit();
 });
